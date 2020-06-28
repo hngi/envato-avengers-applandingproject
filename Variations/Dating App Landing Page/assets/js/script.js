71759026d420e81fc41832cbce3d41190ed34d2f
Orim Dominic <orimdominic@gmail.com>
2020-06-28 09:40:02 -0400

    feat: download button in navbar

--- Variations/Dating App Landing Page/assets/js/script.js
+++ Variations/Dating App Landing Page/assets/js/script.js
@@ -1,3 +1,21 @@
-function animateNavToggler(el) {
-  el.classList.toggle('change');
-}
+// function animateNavToggler(el) {
+//   el.classList.toggle('change');
+// }
+
+document.querySelector('[data-nav-toggler]')
+  .addEventListener('click', event => {
+    const navToggler = document.querySelector('.nav-toggler-icon');
+    if (navToggler.classList.contains('change')) {
+      navToggler.classList.remove('change');
+    } else {
+      navToggler.classList.add('change');
+    }
+
+  }, false);
+
+document.querySelectorAll('.nav-item')
+  .forEach((el, key, parent) => {
+    el.addEventListener('click', (event) => {
+
+    }, false);
+  });
