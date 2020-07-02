e044f1fb51444333fed6cb3149564c56c62dbb8e
Netacci <52791481+Netacci@users.noreply.github.com>
2020-07-02 11:57:37 +0100

    fixed border

--- Variations/Health App Landing Page/assets/js/healthapp.js
+++ Variations/Health App Landing Page/assets/js/healthapp.js
@@ -8,7 +8,3 @@ window.addEventListener("scroll", () => {
   }
 });
 $(function() { AOS.init({ offset: 300, duration:1000, easing:"ease-out-quad", once:0 });  window.addEventListener('load', AOS.refresh);});
-<<<<<<< HEAD
-=======
-
->>>>>>> 7e565eafb664a2fbd4ec255345cac2400749a693
